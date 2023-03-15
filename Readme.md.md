cd1a6af2d5a703845d16d5e8b7c5bb1ec90a6b0a
Mai Nguyen <nguyhienmai@gmail.com>
2023-03-15 10:39:00 +0100

    création fichier readme email_fr liste_email

---
+++ Readme.md.md
@@ -0,0 +1,21 @@
+# Liste d'adresse email finissant par fr
+
+### Description
+
+Il s'agit d'un court programme python pour trouver la liste des emails français qui se terminent par .fr dans un fichier texte
+
+### Installation requise
+
+- Python
+- Module Python re (regex)
+
+### Exécution du code de l’application :
+
+Commencez tout d'abord par installer Python. Lancez ensuite la console, placez vous dans le dossier de votre choix puis clonez ce repository: 
+- git clone https://github.com/Mai1213/ECF1.git
+
+Vous pouvez enfin lancer le script:
+- email_fr.py
+
+
+
---
+++ email_fr.py
@@ -0,0 +1,23 @@
+import re
+
+fichier = "C:/Users/Administrateur/Downloads/liste_email.txt"
+
+
+def extraire_emails_francais(fichier):
+    emails_francais = []
+    pattern = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(?:[a-zA-Z]{2,}|\d{1,3})"
+
+    with open(fichier, 'r', encoding='utf-8') as file:
+        contenu = file.read()
+        emails = re.findall(pattern, contenu)
+
+        for email in emails:
+            if email.endswith('.fr'):
+                emails_francais.append(email)
+
+    return emails_francais
+
+
+emails_francais = extraire_emails_francais(fichier)
+print("Liste des emails français:")
+print(emails_francais)
